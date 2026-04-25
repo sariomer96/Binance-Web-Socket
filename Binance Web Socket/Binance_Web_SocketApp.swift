3255cd110290f9e4384dc3c6d4aefc0799958d4f
omer <46028769+sariomer96@users.noreply.github.com>
2026-04-25 13:07:33 +0300

    Initial Commit

---
+++ Binance Web Socket/Binance_Web_SocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  Binance_Web_SocketApp.swift
+//  Binance Web Socket
+//
+//  Created by Omer on 25.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Binance_Web_SocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
